eaa701d7fab86beb3ebfa85bf564cba4cd9a39a3
nana-47 <nana.oikawa@rakus-partners.co.jp>
2022-09-09 20:15:03 +0900

    基本機能の実装

--- src/main/java/com/example/service/TodoService.java
+++ src/main/java/com/example/service/TodoService.java
@@ -1,5 +1,10 @@
 package com.example.service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +20,77 @@ public class TodoService {
 	private TodoMapper todoMapper;
 
 	/**
+	 * TODO情報の一覧を取得
+	 * 
+	 * @param
+	 * @return todoMap
+	 */
+	public Map<String, List<Todo>> allTodo() {
+
+		List<Todo> todoList = todoMapper.findByTodo();
+		List<Todo> finishList = todoMapper.findByFinishedTodo();
+
+		Map<String, List<Todo>> todoMap = new HashMap<>();
+		todoMap.put("todoList", todoList);
+		todoMap.put("finishList", finishList);
+		return todoMap;
+	}
+
+	/**
 	 * TODO情報の追加と最新のTODOリストを返す
 	 * 
 	 * @param todo
 	 * @return todoMap
 	 */
-	public void addTodo(Todo todo) {
+	public Map<String, List<Todo>> addTodo(Todo todo) {
+
 		todoMapper.insertTodo(todo);
+
+		List<Todo> todoList = todoMapper.findByTodo();
+
+		Map<String, List<Todo>> todoMap = new HashMap<>();
+		todoMap.put("todoList", todoList);
+		return todoMap;
+	}
+
+	/**
+	 * 該当TODOの削除
+	 * 
+	 * @param
+	 * @return todoMap
+	 */
+	public Map<String, List<Todo>> deleteTodo(Integer id) {
+
+		todoMapper.deleteTodo(id);
+
+		List<Todo> todoList = todoMapper.findByTodo();
+		List<Todo> finishList = todoMapper.findByFinishedTodo();
+
+		Map<String, List<Todo>> todoMap = new HashMap<>();
+		todoMap.put("todoList", todoList);
+		todoMap.put("finishList", finishList);
+		return todoMap;
+	}
+
+
+	/**
+	 * 該当TODOを完了にする
+	 * 
+	 * @param
+	 * @return todoMap
+	 */
+	public Map<String, List<Todo>> finishTodo(Integer id) {
+
+		Date date = new Date();
+
+		todoMapper.updateTodo(id, date);
+
+		List<Todo> todoList = todoMapper.findByTodo();
+		List<Todo> finishList = todoMapper.findByFinishedTodo();
+
+		Map<String, List<Todo>> todoMap = new HashMap<>();
+		todoMap.put("todoList", todoList);
+		todoMap.put("finishList", finishList);
+		return todoMap;
 	}
 }
